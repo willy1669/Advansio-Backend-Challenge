121b2857532c9ea4fc6a00c1243b9b1944b2e5ed
chinonso Wiliiams <williamschinonso11gmail.com>
2020-06-17 17:24:27 +0100

    create token validator

---
+++ server/middlewares/verifyToken.js
@@ -0,0 +1,35 @@
+import validations from '../helpers/validations.js';
+
+/**
+ * @method verifyToken
+ * @description Verifies the token provided by the user
+ * @param {*} req
+ * @param {*} res
+ * @returns {*} - JSON response object
+ */
+
+const verifyToken = (req, res, next) => {
+  const payload = validations.verifyAuthHeader(req);
+  let error;
+  let status;
+  if (!payload || payload.error === 'error') {
+    status = 401;
+    error = 'You are not authorized';
+  }
+  if (payload.error === 'Invalid token') {
+    status = 403;
+    error = 'Forbidden';
+  }
+
+  if (error) {
+    return res.status(status).json({
+      errors: {
+        body: [error],
+      },
+    });
+  }
+  req.user = payload;
+  return next();
+};
+
+export default verifyToken;
