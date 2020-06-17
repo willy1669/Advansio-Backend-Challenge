39ce14ca98124a01f5b36164ea8b66435169b0fd
chinonso Wiliiams <williamschinonso11gmail.com>
2020-06-17 17:20:32 +0100

    export all validators

---
+++ server/middlewares/index.js
@@ -0,0 +1,11 @@
+import trimmerMiddleware from './trimmer.middlewares.js';
+import loginMiddleware from './login.js';
+import registerMiddleware from './register.js';
+import verifyToken from './verifyToken.js';
+
+export default {
+  trimmerMiddleware,
+  loginMiddleware,
+  registerMiddleware,
+  verifyToken,
+};
