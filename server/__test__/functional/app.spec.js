ac753f93f9d69128b1a4736727a20c9ae331ce16
chinonso Wiliiams <williamschinonso11gmail.com>
2020-06-19 11:09:55 +0100

    configure mocha and chai for server

---
+++ server/__test__/functional/app.spec.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app.js';
+
+//cofigure chai to use chai http
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+
+
